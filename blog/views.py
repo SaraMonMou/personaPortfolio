bd3ad6ac34e8e58f1dbb814948d9899adc370526
SaraMonMou <a23saramm@iessanclemente.net>
2024-11-14 14:28:19 +0100

    cambios en blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import blog
 
 # Create your views here.
+
+def all_blogs(request):
+    blog_ = blog.objects.all()
+    return render(request, 'blog/all_blogs.html', {'blog':blog_})
