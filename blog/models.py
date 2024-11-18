28203b3562b8ee8d08b388d5185523c91613a4d0
SaraMonMou <a23saramm@iessanclemente.net>
2024-11-18 10:58:41 +0100

    cambios en blog

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class blog(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from .models import blog
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
 
 def all_blogs(request):
-    blog_ = blog.objects.all()
+    blog_ = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blog':blog_})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':blog})
