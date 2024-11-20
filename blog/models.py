70a36539066fcd22633f982f8ce4c7e4bc5a3c37
SaraMonMou <a23saramm@iessanclemente.net>
2024-11-20 10:45:05 +0100

    proxecto rematado

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     date = models.DateField()
+    image = models.ImageField(upload_to='portfolio/images/', blank=True)
+    imageSec = models.ImageField(upload_to='portfolio/images/', blank=True)
+
 
     def __str__(self):
         return self.title
